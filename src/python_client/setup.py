ba33a73e92cc949dd0d4cc3fb6928deb0394e72c
George Dias <gdias@mitre.org>
2023-05-21 16:07:20 -0500

    udates to the python generator

--- src/python_client/setup.py
+++ src/python_client/setup.py
@@ -13,7 +13,7 @@
 from setuptools import setup, find_packages  # noqa: H301
 
 NAME = "emass_client_api"
-VERSION = "3.9.0"
+VERSION = "3.8.0"
 # To install the library, run the following
 #
 # python setup.py install
